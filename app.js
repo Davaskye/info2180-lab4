47e654a5b265b2cbce892c0bc7f5f37d0486de66
Davaskye DeBique <Davaskye@gmail.com>
2021-10-31 18:23:02 -0400

    Popup now only works on button press. Added search bar.

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 window.onload = function() {
     let searchButton = document.getElementsByClassName("btn");
-    console.log(searchButton[0]);
-
 
     searchButton[0].onclick = function(){
-        doSomething();
+        httpRequest.send();
     }
 }
 
@@ -13,7 +11,7 @@ var httpRequest = new XMLHttpRequest();
 var url = "http://localhost/info2180-lab4/superheroes.php"; 
 httpRequest.onreadystatechange = doSomething; 
 httpRequest.open('GET', url); 
-httpRequest.send();
+
 
 function doSomething() { 
     if (httpRequest.readyState === XMLHttpRequest.DONE) { 
